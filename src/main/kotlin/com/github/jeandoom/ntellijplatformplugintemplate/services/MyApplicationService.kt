f772bb8d0505fc8885ed81b3d0372abeaedf757b
GitHub Action <action@github.com>
2021-10-11 09:58:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jeandoom/ntellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jeandoom.ntellijplatformplugintemplate.services
+
+import com.github.jeandoom.ntellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jeandoom/ntellijplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jeandoom/ntellijplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jeandoom.ntellijplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jeandoom.ntellijplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
